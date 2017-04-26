d2730fb6eee6f88a782493846ed694a1e50254a4
nosagielang <nosagie.lang@gmail.com>
2017-04-25 23:57:05 -0400

    Create complicated output with variable.py

---
+++ complicated output with variable.py
@@ -0,0 +1,10 @@
+FirstName= "Bob"
+LastName = "Newhard"
+
+pay_rate = 25.25
+
+hours_worked = 45
+
+total = pay_rate*hours_worked
+
+print "Employee",  FirstName, LastName, "has worked", hours_worked, "hours at $"+str(pay_rate), "per hour for a total of $"+str(total), "pay."
